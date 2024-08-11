a0c136f61cb8c86889dc02be8a41f289f5c34f46
Kean H Lau <khklau@users.noreply.github.com>
2024-08-11 21:10:01 +1000

    Adding basic logic to drive the car along the middle of the track at 30% acceleration.

--- src/lib.cpp
+++ src/lib.cpp
@@ -110,15 +110,26 @@ static void drive(
     tSituation* s)
 {
     memset(&car->ctrl, 0, sizeof(tCarCtrl));
-    car->ctrl.brakeCmd = 1.0; /* all brakes on ... */
-    /*
-     * add the driving code here to modify the
-     * car->_steerCmd
-     * car->_accelCmd
-     * car->_brakeCmd
-     * car->_gearCmd
-     * car->_clutchCmd
-     */
+
+    // relative angle of current track segment tangent to the middle line of starting segment
+    float track_seg_relative_angle = RtTrackSideTgAngleL(&(car->pub.trkPos));
+
+    // relative angle of car yaw to the the middle line of starting segment
+    float car_yaw_relative_angle = track_seg_relative_angle - car->pub.DynGC.pos.az;
+
+    // adjust the relative car yaw to middle line to ensure it is within 2 PI
+    car_yaw_relative_angle = remainder(car_yaw_relative_angle, 2 * PI);
+
+    // ratio of distance from car to middle relative to the segment width
+    float middle_offset_to_width_ratio = car->pub.trkPos.toMiddle / car->pub.trkPos.seg->width;
+
+    float steering_adjustment = (car_yaw_relative_angle - middle_offset_to_width_ratio) / car->info.steerLock;
+
+    car->ctrl.steer = steering_adjustment;
+    car->ctrl.accelCmd = 0.3;   // 30% acceleration
+    car->ctrl.brakeCmd = 0.0;   // no brakes
+    car->ctrl.gear = 1;         // drive in 1st gear
+    car->ctrl.clutchCmd = 0.0;  // clutch pedal disengaged
 }
 
 /**
